6b86cebf31af84b359b8270a59040999ef64807d
MumandamNadine <143093141+MumandamNadine@users.noreply.github.com>
2024-07-03 23:18:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// Function to handle search on index.html page
+function search_page(event) {
+  event.preventDefault();
+  let input = document.getElementById('searchbar').value.toLowerCase();
+  let sections = document.querySelectorAll('.main-page, .animals > div');
+
+  sections.forEach(section => {
+      let text = section.innerText.toLowerCase();
+      if (text.includes(input)) {
+          section.style.display = 'block';
+      } else {
+          section.style.display = 'none';
+      }
+  });
+}
+
+function search_animal(event) {
+  event.preventDefault();
+  let input = document.getElementById('searchbar').value.toLowerCase();
+  let cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+      let title = card.querySelector('.card-title').innerText.toLowerCase();
+      if (title.includes(input)) {
+          card.style.display = 'block';
+      } else {
+          card.style.display = 'none';
+      }
+  });
+}
+
+
+
+
